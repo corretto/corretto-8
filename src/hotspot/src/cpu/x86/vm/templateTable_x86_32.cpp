24febf8cce68be92c60acdab2431978a99b48f64
Xin Liu <xxinliu@amazon.com>
2019-03-21 17:28:33 -0700

    Revert "[C8-16] Back port from OpenJDK 12, fixing JDK-8206075: "On x86, assert on unbound assembler Labels used as branch targets"."
    This reverts commit b6c465af0feadd7b5e8d787740b3005860c358e1.

--- src/hotspot/src/cpu/x86/vm/templateTable_x86_32.cpp
+++ src/hotspot/src/cpu/x86/vm/templateTable_x86_32.cpp
@@ -1640,15 +1640,15 @@ void TemplateTable::branch(bool is_jsr, bool is_wide) {
         // Increment the MDO backedge counter
         const Address mdo_backedge_counter(rbx, in_bytes(MethodData::backedge_counter_offset()) +
                                                 in_bytes(InvocationCounter::counter_offset()));
-        __ increment_mask_and_jump(mdo_backedge_counter, increment, mask, rax, false, Assembler::zero,
-                                   UseOnStackReplacement ? &backedge_counter_overflow : NULL);
+        __ increment_mask_and_jump(mdo_backedge_counter, increment, mask,
+                                   rax, false, Assembler::zero, &backedge_counter_overflow);
         __ jmp(dispatch);
       }
       __ bind(no_mdo);
       // Increment backedge counter in MethodCounters*
       __ movptr(rcx, Address(rcx, Method::method_counters_offset()));
-      __ increment_mask_and_jump(Address(rcx, be_offset), increment, mask, rax, false, Assembler::zero,
-                                 UseOnStackReplacement ? &backedge_counter_overflow : NULL);
+      __ increment_mask_and_jump(Address(rcx, be_offset), increment, mask,
+                                 rax, false, Assembler::zero, &backedge_counter_overflow);
     } else {
       // increment counter
       __ movptr(rcx, Address(rcx, Method::method_counters_offset()));
