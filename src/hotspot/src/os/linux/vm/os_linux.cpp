11da6eb44a2f05aa3fea5619f3e62cc8a8765f7e
Volker Simonis <simonisv@amazon.com>
2020-03-09 18:37:44 +0100

    Revert part of "IcedTea 3.14.0" integration which causes serious performance regression
    Integration of "IcedTea 3.14.0" unintentionally integrated a Spectre mitigation which seriously impacts performance on specific benchmarks. This changes reverts that IcedTea only feature.

--- src/hotspot/src/os/linux/vm/os_linux.cpp
+++ src/hotspot/src/os/linux/vm/os_linux.cpp
@@ -103,8 +103,6 @@
 # include <inttypes.h>
 # include <sys/ioctl.h>
 
-#include <sys/prctl.h>
-
 PRAGMA_FORMAT_MUTE_WARNINGS_FOR_GCC
 
 #ifndef _GNU_SOURCE
@@ -4997,48 +4995,6 @@ const char* os::exception_name(int exception_code, char* buf, size_t size) {
   } else {
     return NULL;
   }
-}
-
-/* Per task speculation control */
-#ifndef PR_GET_SPECULATION_CTRL
-# define PR_GET_SPECULATION_CTRL    52
-#endif
-#ifndef PR_SET_SPECULATION_CTRL
-# define PR_SET_SPECULATION_CTRL    53
-#endif
-/* Speculation control variants */
-#ifndef PR_SPEC_STORE_BYPASS
-# define PR_SPEC_STORE_BYPASS          0
-#endif
-/* Return and control values for PR_SET/GET_SPECULATION_CTRL */
-
-#ifndef PR_SPEC_NOT_AFFECTED
-# define PR_SPEC_NOT_AFFECTED          0
-#endif
-#ifndef PR_SPEC_PRCTL
-# define PR_SPEC_PRCTL                 (1UL << 0)
-#endif
-#ifndef PR_SPEC_ENABLE
-# define PR_SPEC_ENABLE                (1UL << 1)
-#endif
-#ifndef PR_SPEC_DISABLE
-# define PR_SPEC_DISABLE               (1UL << 2)
-#endif
-#ifndef PR_SPEC_FORCE_DISABLE
-# define PR_SPEC_FORCE_DISABLE         (1UL << 3)
-#endif
-#ifndef PR_SPEC_DISABLE_NOEXEC
-# define PR_SPEC_DISABLE_NOEXEC        (1UL << 4)
-#endif
-
-static void set_speculation() __attribute__((constructor));
-static void set_speculation() {
-  if ( prctl(PR_SET_SPECULATION_CTRL,
-             PR_SPEC_STORE_BYPASS,
-             PR_SPEC_DISABLE_NOEXEC, 0, 0) == 0 ) {
-    return;
-  }
-  prctl(PR_SET_SPECULATION_CTRL, PR_SPEC_STORE_BYPASS, PR_SPEC_DISABLE, 0, 0);
 }
 
 // this is called _before_ most of the global arguments have been parsed
