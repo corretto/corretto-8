233cb4670c4b77a8ca92697f902dfeea78bdac83
David Alvarez <alvdavi@amazon.com>
2020-09-30 05:43:15 +0000

    8242680: Improved URI Support
    Reviewed-by: alanb, rhalade

--- src/jdk/src/solaris/classes/sun/nio/fs/UnixUriUtils.java
+++ src/jdk/src/solaris/classes/sun/nio/fs/UnixUriUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -115,9 +115,10 @@ class UnixUriUtils {
         // trailing slash if directory
         if (sb.charAt(sb.length()-1) != '/') {
             try {
+                 up.checkRead();
                  if (UnixFileAttributes.get(up, true).isDirectory())
                      sb.append('/');
-            } catch (UnixException x) {
+            } catch (SecurityException | UnixException x) {
                 // ignore
             }
         }
