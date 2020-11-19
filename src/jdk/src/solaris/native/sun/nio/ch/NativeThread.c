c106ba38b618dab3bfba155df0c59ddfb9806ad4
Clive Verghese <verghese@amazon.com>
2020-11-19 20:28:03 +0000

    8252248: __SIGRTMAX is not declared in musl libc
    Reviewed-by: alanb, vtewari, stuefe

--- src/jdk/src/solaris/native/sun/nio/ch/NativeThread.c
+++ src/jdk/src/solaris/native/sun/nio/ch/NativeThread.c
@@ -31,18 +31,17 @@
 #include "jlong.h"
 #include "sun_nio_ch_NativeThread.h"
 #include "nio_util.h"
+#include <pthread.h>
 
 #ifdef __linux__
-  #include <pthread.h>
   #include <sys/signal.h>
   /* Also defined in net/linux_close.c */
-  #define INTERRUPT_SIGNAL (__SIGRTMAX - 2)
+  #define INTERRUPT_SIGNAL (SIGRTMAX - 2)
 #elif __solaris__
   #include <thread.h>
   #include <signal.h>
   #define INTERRUPT_SIGNAL (SIGRTMAX - 2)
 #elif _ALLBSD_SOURCE
-  #include <pthread.h>
   #include <signal.h>
   /* Also defined in net/bsd_close.c */
   #define INTERRUPT_SIGNAL SIGIO
