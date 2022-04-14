27899c310b16ba30b05e6b5c744af6cd1f9c1a9f
Sergey Bylokhov <bylokhov@amazon.com>
2022-04-14 11:41:19 -0700

    Update amazon-cacerts

--- src/jdk/test/sun/security/lib/cacerts/VerifyCACerts.java
+++ src/jdk/test/sun/security/lib/cacerts/VerifyCACerts.java
@@ -53,7 +53,7 @@ public class VerifyCACerts {
             + File.separator + "security" + File.separator + "cacerts";
 
     // The numbers of certs now.
-    private static final int COUNT = 215;
+    private static final int COUNT = 211;
 
     // SHA-256 of cacerts, can be generated with
     // shasum -a 256 cacerts | sed -e 's/../&:/g' | tr '[:lower:]' '[:upper:]' | cut -c1-95
